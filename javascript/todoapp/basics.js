0449464a6a849d684993c397ed0bec8a2eaec794
kpavankumaar <k.pavankumaar@gmail.com>
2022-01-14 14:58:09 +0530

    todo app part1

---
+++ javascript/todoapp/basics.js
@@ -0,0 +1,4 @@
+// add task action -> 6 elements to be created are div, input type checkbox, lable, input type text, edit-button, delete-button
+// complestionstatusfn -> move the complete task to opposite section ie from completed section to incomplete section, or from incomplete section to complete section 
+// Edit -> requires to display the input type text and hide the label element, after editing hide input text and display label element
+// Delete ->  delete the whole task
